89a6ec0aeed8711fc72a24d553f81f8970d7f2a8
wimzer <wimzerson@gmail.com>
2026-08-15 20:35:50 -0500

    Log planetary mining account reservations

--- engine/server/application/ConnectionServer/src/shared/SessionApiClient.cpp
+++ engine/server/application/ConnectionServer/src/shared/SessionApiClient.cpp
@@ -439,7 +439,10 @@ void SessionApiClient::OnGetFeatures(const apiTrackingNumber trackingNumber,
 
 				int const currentCount = existingFeature ? existingFeature->GetConsumeCount() : 0;
 				bool const isPlanetaryMiningDroidReservation = (adjustAccountFeatureIdRequest->getGameCode() == PlatformGameCode::SWG) && (adjustAccountFeatureIdRequest->getFeatureId() == cms_planetaryMiningDroidFeatureId) && (adjustAccountFeatureIdRequest->getAdjustment() == 1);
-				if (isPlanetaryMiningDroidReservation && (currentCount + ms_pendingPlanetaryMiningDroidReservations[adjustAccountFeatureIdRequest->getTargetStationId()] >= cms_planetaryMiningDroidMaximumJobs))
+				int const pendingReservations = ms_pendingPlanetaryMiningDroidReservations[adjustAccountFeatureIdRequest->getTargetStationId()];
+				if (isPlanetaryMiningDroidReservation)
+					LOG("CustomerService",("PlanetaryMiningDroid: account (%lu) reservation request current (%d) pending (%d)", adjustAccountFeatureIdRequest->getTargetStationId(), currentCount, pendingReservations));
+				if (isPlanetaryMiningDroidReservation && (currentCount + pendingReservations >= cms_planetaryMiningDroidMaximumJobs))
 				{
 					GameConnection * const gc = ConnectionServer::getGameConnection(adjustAccountFeatureIdRequest->getGameServer());
 					if (gc)
